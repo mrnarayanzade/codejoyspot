fdc05ebf8d13a0aef04b3b9ebb64f17e5a84820a
Narayan-Zade <narayan.d.zade@gmail.com>
2023-12-22 12:36:20 +0530

    performed CRUD operations

--- projects/urls.py
+++ projects/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('projects/', views.projects, name='projects'),
-    path('single-project/<str:pk>', views.project, name="single-project" )
+    path('single-project/<str:pk>', views.project, name="single-project" ),
+    path('add-project/', views.addproject, name="addproject"),
+    path('edit-project/<str:pk>', views.editproject, name="editproject"),
+    path('delete-project/<str:pk>', views.deleteproject, name="deleteproject")
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Project, Review, Tag
+from .forms import ProjectForm
 
 # Create your views here.
 def projects(request):
@@ -14,3 +15,33 @@ def project(request, pk):
     tags = projectobj.tags.all()
     context = { 'project': projectobj, 'tags': tags}
     return render(request, 'single-project.html', context)
+
+def addproject(request):
+    form = ProjectForm()
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        form.save()
+        return redirect('projects')
+    context = {'form': form}
+    return render(request, 'project_form.html', context)
+
+def editproject(request, pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance = project)
+
+    if request.method == "POST":
+        form = ProjectForm(request.POST, instance = project)
+        form.save()
+        return redirect('projects')
+    context = {'form': form}
+    return render(request, 'project_form.html', context)
+
+def deleteproject(request , pk):
+    project = Project.objects.get(id=pk)
+    
+    if request.method == "POST":
+        project.delete()
+        return redirect('projects')
+    context = {'object': project}
+    return render(request, 'delete_project.html', context)
+    
